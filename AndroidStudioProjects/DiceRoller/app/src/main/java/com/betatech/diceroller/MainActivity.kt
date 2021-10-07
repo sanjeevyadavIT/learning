5e16ede7428c04b3f87777e40129bd421e77627c
Sanjeev Yadav <sanjeevy133@protonmail.com>
2021-10-07 07:57:27 +0530

    Add roll dice logic

--- AndroidStudioProjects/DiceRoller/app/src/main/java/com/betatech/diceroller/MainActivity.kt
+++ AndroidStudioProjects/DiceRoller/app/src/main/java/com/betatech/diceroller/MainActivity.kt
@@ -2,10 +2,37 @@ package com.betatech.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.betatech.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rollBtn.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(6) + 1
+        binding.diceIv.setImageResource(getDiceImage(randomInt))
+    }
+
+    private fun getDiceImage(randomInt: Int): Int  = when(randomInt) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        6 -> R.drawable.dice_6
+        else -> R.drawable.empty_dice
     }
 }
