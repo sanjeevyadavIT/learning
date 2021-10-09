b1133f0db05e5ba5c8f174b389b99a07b7a69d0b
Sanjeev Yadav <sanjeevy133@protonmail.com>
2021-10-09 07:30:17 +0530

    add on navigate up logic

--- AndroidStudioProjects/Udacity - ud9012/app/src/main/java/com/betatech/ud9012/MainActivity.kt
+++ AndroidStudioProjects/Udacity - ud9012/app/src/main/java/com/betatech/ud9012/MainActivity.kt
@@ -2,6 +2,8 @@ package com.betatech.ud9012
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.betatech.ud9012.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupActionbar()
+    }
+
+    private fun setupActionbar() {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
