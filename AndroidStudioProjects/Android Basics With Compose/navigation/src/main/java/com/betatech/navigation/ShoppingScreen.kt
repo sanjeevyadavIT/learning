dd3fc9995c49df3cb0cd4c004a1a4d70936abf27
Sanjeev Yadav <sanjeevy133@protonmail.com>
2024-06-17 17:13:56 +0530

    Hook AppBar with NavController

--- AndroidStudioProjects/Android Basics With Compose/navigation/src/main/java/com/betatech/navigation/ShoppingScreen.kt
+++ AndroidStudioProjects/Android Basics With Compose/navigation/src/main/java/com/betatech/navigation/ShoppingScreen.kt
@@ -3,32 +3,60 @@ package com.betatech.navigation
 import android.app.Notification.Action
 import android.content.Context
 import android.content.Intent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.betatech.navigation.ui.theme.AndroidBasicsWithComposeTheme
 
-enum class ShoppingRoutes {
-    ProductList,
-    ProductDetail,
-    Cart
+enum class ShoppingRoutes(@StringRes val title: Int) {
+    ProductList(title = R.string.product_list_screen),
+    ProductDetail(title = R.string.product_detail_screen),
+    Cart(title = R.string.cart_screen)
 }
 
 @Composable
 fun ShoppingScreen(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    val context = LocalContext.current
+    // Get current back stack entry
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    // Get the name of the current screen
+    val currentScreen = ShoppingRoutes.valueOf(
+        backStackEntry?.destination?.route ?: ShoppingRoutes.ProductList.name
+    )
+
 
     AndroidBasicsWithComposeTheme {
-        Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
+        Scaffold(
+            topBar = {
+                ShoppingAppBar(
+                    currentScreen = currentScreen,
+                    canNavigateBack = navController.previousBackStackEntry != null,
+                    navigateUp = {
+                        navController.navigateUp()
+                    }
+                )
+            },
+            modifier = modifier.fillMaxSize()
+        ) { innerPadding ->
             NavHost(
                 navController = navController,
                 startDestination = ShoppingRoutes.ProductList.name,
@@ -45,6 +73,8 @@ fun ShoppingScreen(modifier: Modifier = Modifier) {
                     })
                 }
                 composable(ShoppingRoutes.Cart.name) {
+                    val context = LocalContext.current
+
                     CartScreen(
                         onCancelButtonClicked = {
                             cancelOrderAndNavigateToStart(navController)
@@ -57,6 +87,32 @@ fun ShoppingScreen(modifier: Modifier = Modifier) {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShoppingAppBar(
+    currentScreen: ShoppingRoutes,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(text = stringResource(currentScreen.title))
+        },
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack, contentDescription = stringResource(
+                            id = R.string.back
+                        )
+                    )
+                }
+            }
+        }
+    )
 }
 
 private fun cancelOrderAndNavigateToStart(
