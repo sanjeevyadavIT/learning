ddf4531300669167b9ce0a8e47bf86c99866e09f
Sanjeev Yadav <sanjeevy133@gmail.com>
2024-06-28 23:19:22 +0530

    delete unused file

--- AndroidStudioProjects/Android Basics With Compose/mycityapp/src/main/java/com/betatech/mycityapp/ui/MyCityHomeScreen.kt
+++ AndroidStudioProjects/Android Basics With Compose/mycityapp/src/main/java/com/betatech/mycityapp/ui/MyCityHomeScreen.kt
@@ -1,17 +1,7 @@
 package com.betatech.mycityapp.ui
 
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.PermanentDrawerSheet
-import androidx.compose.material3.PermanentNavigationDrawer
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.betatech.mycityapp.ui.utils.MyCityNavigationType
-import com.betatech.mycityapp.ui.utils.NavigationItemContent
 
 @Composable
 fun MyCityHomeScreen(
--- AndroidStudioProjects/Android Basics With Compose/mycityapp/src/main/java/com/betatech/mycityapp/ui/utils/NavigationItemContent.kt
+++
@@ -1,8 +0,0 @@
-package com.betatech.mycityapp.ui.utils
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class NavigationItemContent(
-    val icon: ImageVector,
-    val text: String
-)
